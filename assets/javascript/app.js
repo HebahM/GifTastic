655b7648e35b93377f099e3c5609640762aa91bb
HebahM <56455544+HebahM@users.noreply.github.com>
2020-01-12 15:30:57 -0600

    added function to start/stop gifs on click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -34,7 +34,7 @@ $("#addAnimal").on("click", function () {
 $(document).on("click", ".animal-btn", function () {
     $("#gifsGoHere").empty();
     var buttonText = $(this).text();
-    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=CRayK7TqalIsNLe4qFEfH2Eandlo4sDY&limit=10";
+    var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=CRayK7TqalIsNLe4qFEfH2Eandlo4sDY&limit=10";
 
     $.ajax({
         url: giphyURL,
@@ -43,14 +43,34 @@ $(document).on("click", ".animal-btn", function () {
         .then(function (response) {
             console.log(response.data)
             for (var i = 0; i < response.data.length; i++) {
-                var pRating = $("<p>").text("Rating: " + response.data[i].rating)
-                var pTitle = $("<p>").text("Title: " + response.data[i].title)
-                var pic = $("<img>")
-                pic.attr("src", response.data[i].images.fixed_height.url)
+                var pRating = $("<p>").text("Rating: " + response.data[i].rating);
+                var pTitle = $("<p>").text("Title: " + response.data[i].title);
+                var pic = $("<img>").attr("src", response.data[i].images.fixed_height_still.url);
+                pic.attr("data-still", response.data[i].images.fixed_height_still.url);
+                pic.attr("data-animate", response.data[i].images.fixed_height.url);
+                pic.attr("data-state", "still");
+                pic.addClass("gif");
+                //pic.attr("data-state", "still")
+
                 $("#gifsGoHere").append(pTitle);
                 $("#gifsGoHere").append(pRating);
                 $("#gifsGoHere").append(pic);
+
             }
+
+            $(".gif").on("click", function() {
+                var state = $(this).attr("data-state");
+                if (state === "still") {
+                    $(this).attr("src", $(this).attr("data-animate"));
+                    $(this).attr("data-state", "animate");
+                  } else {
+                    $(this).attr("src", $(this).attr("data-still"));
+                    $(this).attr("data-state", "still");
+                  }
+            })
+
         })
 })
 
+
+// change still to animate and vice versa on gif click
