1c9ecdb3c9eff4a926b6037b4f17eab355f07c16
HebahM <56455544+HebahM@users.noreply.github.com>
2020-01-12 12:54:15 -0600

    adding data from giphy API

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,63 +1,55 @@
-var buttons = ["Cat", "Dog", "Mouse", "Lion", "Rabbit", "Frog"];
-var buttonText;
-
-// make buttons out of array -- for loop?
-// add user input to array
-// clear array?
+var topics = ["Cat", "Dog", "Mouse", "Lion", "Rabbit", "Frog"];
 
 
-//////////// API
-// create query URL:
-//      concatenate button value into url
-//      limit results to 10 gifs
-//      add details for rating, and other details
-//      put results in #gifsGoHere
-//      
 
-
-// Function for displaying movie data
+// function to create buttons from array
 function renderButtons() {
 
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
+
     $("#buttons").empty();
-
-    // Looping through the array of movies
-    for (var i = 0; i < buttons.length; i++) {
-
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+    for (var i = 0; i < topics.length; i++) {
         var a = $("<button>");
-        // Adding a class of movie-btn to our button
         a.addClass("animal-btn btn btn-info");
-        // Adding a data-attribute
-        a.attr("data-name", buttons[i]);
-        // Providing the initial button text
-        a.text(buttons[i]);
-        // Adding the button to the buttons-view div
+        a.attr("data-name", topics[i]);
+        a.text(topics[i]);
         $("#buttons").append(a);
     }
 }
 
-function displayGIFs() {
-    var giphyURL = "http://api.giphy.com/v1/gifs/search?q=cat&api_key=CRayK7TqalIsNLe4qFEfH2Eandlo4sDY&limit=10";
 
-}
 
+// create buttons
 renderButtons();
+
+// add buttons to array and append to DOM
 $("#addAnimal").on("click", function () {
     console.log($("input").val())
-    buttons.push($("input").val())
+    topics.push($("input").val())
     renderButtons();
 })
 
-$(".animal-btn").on("click", function () {
-    buttonText = $(this).text()
-    console.log(buttonText)
 
+// Function for displaying gifs and data
+
+$(document).on("click", ".animal-btn", function () {
+    $("#gifsGoHere").empty();
+    var buttonText = $(this).text();
+    var giphyURL = "https://api.giphy.com/v1/gifs/search?q=" + buttonText + "&api_key=CRayK7TqalIsNLe4qFEfH2Eandlo4sDY&limit=10";
+
+    $.ajax({
+        url: giphyURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response.data)
+            for (var i = 0; i < response.data.length; i++) {
+                var pRating = $("<p>").text("Rating: " + response.data[i].rating)
+                var pTitle = $("<p>").text("Title: " + response.data[i].title)
+                var pic = $("<img>").attr("src", response.data[i].images.fixed_height_still.url)
+                $("#gifsGoHere").append(pRating);
+                $("#gifsGoHere").append(pTitle);
+                $("gifsGoHere").append(pic);
+            }
+        })
 })
-
-displayGIFs();
-
-
 
